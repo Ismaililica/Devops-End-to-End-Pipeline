396c157acf0f2da5cbf7734790c31ba3dba8d4c2
Ismail Ilıca <ismaililica73@gmail.com>
2026-01-31 13:19:16 +0300

    open security group egress to all IPs

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -37,27 +37,14 @@ resource "aws_security_group" "ssh-sg" {
 
   egress {
 
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
 
   }
 
-  egress {
-
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 53
-    to_port     = 53
-    protocol    = "udp"
-    cidr_blocks = ["10.1.0.2/32"]
-  }
+ 
 
   tags = {
     Name = "ssh-sg"
